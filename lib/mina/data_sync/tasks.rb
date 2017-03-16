19fd5f54f564ad4fbdf020fb65faf3b2f9adb5b7
Kim Lima <kimlima9@gmail.com>
2017-03-16 11:17:44 -0300

    Not dangerous enough.

--- lib/mina/data_sync/tasks.rb
+++ lib/mina/data_sync/tasks.rb
@@ -5,28 +5,8 @@ namespace :data_sync do
     end
   end
 
-  task :dump_local do
-    run :local do
-      dump_restore('.', fetch(:local_backup_path), mode: :dump, backend: :local)
-    end
-  end
-
-  task :copy_local_to_remote do
-    rsync_db(mode: :local_to_remote)
-  end
-
   task :copy_remote_to_local do
     rsync_db(mode: :remote_to_local)
-  end
-
-  task :restore_remote do
-    exit unless TTY::Prompt.new.yes?("Are you sure you want to restore #{fetch(:rails_env)} database?") do |q|
-      q.suffix ('y/N')
-      q.default false
-    end
-    run :remote do
-      dump_restore(fetch(:current_path), fetch(:remote_backup_path), mode: :restore, backend: :remote)
-    end
   end
 
   task :restore_local do
@@ -39,11 +19,5 @@ namespace :data_sync do
     invoke :'data_sync:dump_remote'
     invoke :'data_sync:copy_remote_to_local'
     invoke :'data_sync:restore_local'
-  end
-
-  task :push do
-    invoke :'data_sync:dump_local'
-    invoke :'data_sync:copy_local_to_remote'
-    invoke :'data_sync:restore_remote'
   end
 end
